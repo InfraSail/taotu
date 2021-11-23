ecde13e2ad922883f522bc05ed86213755f52aa7
Sigma711 <1979934715@qq.com>
2021-11-23 16:50:10 +0000

    Create the log module

---
+++ taotu/logging.cc
@@ -0,0 +1,12 @@
+/**
+ * @file logging.cc
+ * @author Sigma711 (sigma711@foxmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include "logging.h"
+// TODO:
---
+++ taotu/logging.h
@@ -0,0 +1,14 @@
+/**
+ * @file logging.h
+ * @author Sigma711 (sigma711@foxmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef TAOTU_TAOTU_LOGGING_H_
+#define TAOTU_TAOTU_LOGGING_H_
+// TODO:
+#endif
