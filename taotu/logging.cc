219e835219c8e13ce4e7add7b2ba039a44f31b02
Sigma711 <1979934715@qq.com>
2021-11-25 01:03:26 +0000

    Clang-Format

--- taotu/logging.cc
+++ taotu/logging.cc
@@ -1,18 +1,13 @@
 /**
  * @file logging.cc
  * @author Sigma711 (sigma711@foxmail.com)
- * @brief 
+ * @brief
  * @date 2021-11-23
- * 
+ *
  * @license: MIT
  * @copyright Copyright (c) 2021 Sigma711
- * 
+ *
  */
 #include "logging.h"
 
-namespace taotu
-{
-
-
-
-}
+namespace taotu {}
--- taotu/logging.h
+++ taotu/logging.h
@@ -1,21 +1,16 @@
 /**
  * @file logging.h
  * @author Sigma711 (sigma711@foxmail.com)
- * @brief 
+ * @brief
  * @date 2021-11-23
- * 
+ *
  * @license: MIT
  * @copyright Copyright (c) 2021 Sigma711
- * 
+ *
  */
 #ifndef TAOTU_TAOTU_LOGGING_H_
 #define TAOTU_TAOTU_LOGGING_H_
 
-namespace taotu
-{
-
-
-
-}
+namespace taotu {}
 
 #endif
