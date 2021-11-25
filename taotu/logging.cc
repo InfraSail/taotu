a86b3fb9ad42b0588a9927fbb6043b565cae1f7a
Sigma711 <1979934715@qq.com>
2021-11-25 21:52:01 +0000

    Develop Logger

--- taotu/logging.cc
+++ taotu/logging.cc
@@ -10,9 +10,44 @@
  */
 #include "logging.h"
 
+#include <utility>
+
 namespace taotu {
+
+logger::Logger::LoggerPtr logger::Logger::logger_ = nullptr;
+std::mutex logger::Logger::log_mutex_;
+bool logger::Logger::is_initialized = flase;
+
 namespace logger {
+
+bool Logger::StartLogger(const std::string&& log_file) {}
+void Logger::EndLogger() {}
+
+Logger::LoggerPtr Logger::GetLogger() {}
+
+void Logger::RecordLogs(LogLevel log_type, const char* log_info) {
+  RecordLogs(std::move(Log_level_info_prefix[log_type] + log_info));
+}
+void Logger::RecordLogs(LogLevel log_type, const std::string& log_info) {
+  RecordLogs(std::move(Log_level_info_prefix[log_type] + log_info));
+}
+void Logger::RecordLogs(LogLevel log_type, std::string&& log_info) {
+  RecordLogs(std::move(Log_level_info_prefix[log_type] + log_info));
+}
+
+void Logger::RecordLogs() {}
+void Logger::UpdateLoggerTime() {}
+
+void Logger::RecordLogs(const char* log_info) {
+  RecordLogs(std::string(log_info));
+}
+void Logger::RecordLogs(const std::string& log_info) {
+  RecordLogs(std::move(log_info));
+}
+void Logger::RecordLogs(std::string&& log_info) {}
+
 Logger::Logger() {}
 Logger::~Logger() {}
+
 }  // namespace logger
 }  // namespace taotu
--- taotu/logging.h
+++ taotu/logging.h
@@ -11,11 +11,14 @@
 #ifndef TAOTU_TAOTU_LOGGING_H_
 #define TAOTU_TAOTU_LOGGING_H_
 
-#include <non_copyable_movable.h>
-
+#include <memory>
+#include <mutex>
 #include <string>
 
+#include "non_copyable_movable.h"
+
 namespace taotu {
+namespace logger {
 
 // relevant to Log_level_info_prefix
 enum LogLevel {
@@ -23,7 +26,6 @@ enum LogLevel {
   KWarn,
   KError,
 };
-namespace logger {
 namespace {
 // relevant to LogLevel
 std::string Log_level_info_prefix[3]{
@@ -35,11 +37,34 @@ std::string Log_level_info_prefix[3]{
  *
  */
 class Logger : utility::NonCopyableMovable {
- private:
+ public:
+  typedef std::unique_ptr<Logger> LoggerPtr;
+
+  bool StartLogger(const std::string&& log_file);
+  void EndLogger();
+  static LoggerPtr GetLogger();
+
+  void RecordLogs(LogLevel log_type, const char* log_info);
+  void RecordLogs(LogLevel log_type, const std::string& log_info);
+  void RecordLogs(LogLevel log_type, std::string&& log_info);
+
+ protected:
   Logger();
   ~Logger();
+
+ private:
+  void UpdateLoggerTime();
+  void RecordLogs();  // Finally call it when write down logs
+
+  void RecordLogs(const char* log_info);
+  void RecordLogs(const std::string& log_info);
+  void RecordLogs(std::string&& log_info);
+
+  static LoggerPtr logger_;
+  static std::mutex log_mutex_;
+  static bool is_initialized;
 };
 }  // namespace logger
 }  // namespace taotu
 
-#endif  //
+#endif  // !TAOTU_TAOTU_LOGGING_H_
