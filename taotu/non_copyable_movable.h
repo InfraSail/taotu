f53e9d9eb6d73141ffc45a9da239a63ae712dde2
Sigma711 <1979934715@qq.com>
2021-11-25 20:31:27 +0000

    Develop NonCopyableMovable Parent Class

---
+++ taotu/non_copyable_movable.h
@@ -0,0 +1,29 @@
+/**
+ * @file non_copyable_movable.h
+ * @author Sigma711 (sigma711@foxmail.com)
+ * @brief
+ * @date 2021-11-25
+ *
+ * @license: MIT
+ * @copyright Copyright (c) 2021
+ *
+ */
+#ifndef TAOTU_TAOTU_NON_COPYABLE_MOVABLE_H_
+#define TAOTU_TAOTU_NON_COPYABLE_MOVABLE_H_
+
+namespace taotu {
+namespace utility {
+class NonCopyableMovable {
+ protected:
+  NonCopyableMovable() = default;
+  ~NonCopyableMovable() = default;
+
+ private:
+  NonCopyableMovable(const NonCopyableMovable&) = delete;
+  NonCopyableMovable& operator=(const NonCopyableMovable&) = delete;
+  NonCopyableMovable(const NonCopyableMovable&&) = delete;
+};
+}  // namespace utility
+}  // namespace taotu
+
+#endif  // !TAOTU_TAOTU_NON_COPYABLE_MOVABLE_H_
