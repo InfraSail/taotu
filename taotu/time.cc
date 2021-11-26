1c5b2cf14be5d71268b711444181d5e62c27a9cc
Sigma711 <1979934715@qq.com>
2021-11-26 01:32:04 +0000

    Create Time module

---
+++ taotu/time.cc
@@ -0,0 +1,12 @@
+/**
+ * @file time.cc
+ * @author Sigma711 (sigma711@foxmail.com)
+ * @brief
+ * @date 2021-11-26
+ *
+ * @license: MIT
+ * @copyright Copyright (c) 2021 Sigma711
+ *
+ */
+
+namespace taotu {}  // namespace taotu
---
+++ taotu/time.h
@@ -0,0 +1,16 @@
+/**
+ * @file time.h
+ * @author Sigma711 (sigma711@foxmail.com)
+ * @brief
+ * @date 2021-11-26
+ *
+ * @license: MIT
+ * @copyright Copyright (c) 2021 Sigma711
+ *
+ */
+#ifndef TAOTU_TAOTU_TIME_H_
+#define TAOTU_TAOTU_TIME_H_
+
+namespace taotu {}  // namespace taotu
+
+#endif  // !TAOTU_TAOTU_TIME_H_
