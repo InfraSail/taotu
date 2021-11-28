3f280ade19939b7232a0fcafe650282efe52defb
Sigma711 <1979934715@qq.com>
2021-11-28 22:07:49 +0000

    Develop Logger

--- taotu/file.h
+++ taotu/file.h
@@ -1,15 +1,15 @@
-///**
-// * @file file.h
-// * @author Sigma711 (sigma711@foxmail.com)
-// * @brief
-// * @date 2021-11-28
-// *
-// * @license: MIT
-// * @copyright Copyright (c) 2021 Sigma711
-// *
-// */
-//#ifndef TAOTU_TAOTU_FILE_H_
-//
-//namespace taotu {}  // namespace taotu
-//
-//#endif  // !TAOTU_TAOTU_FILE_H_
+/**
+ * @file file.h
+ * @author Sigma711 (sigma711@foxmail.com)
+ * @brief
+ * @date 2021-11-28
+ *
+ * @license: MIT
+ * @copyright Copyright (c) 2021 Sigma711
+ *
+ */
+#ifndef TAOTU_TAOTU_FILE_H_
+
+namespace taotu {}  // namespace taotu
+
+#endif  // !TAOTU_TAOTU_FILE_H_
