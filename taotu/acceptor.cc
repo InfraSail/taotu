9823124e00c16952dbc45127ab26b92325808d2d
Sigma711 <1979934715@qq.com>
2021-12-03 06:23:01 +0000

    Develop Accpetor module

---
+++ taotu/acceptor.cc
@@ -0,0 +1,9 @@
+/**
+ * @file acceptor.cc
+ * @author Sigma711 (sigma711@foxmail.com)
+ * @brief  // TODO:
+ * @date 2021-12-03
+ *
+ * @copyright Copyright (c) 2021 Sigma711
+ *
+ */
---
+++ taotu/acceptor.h
@@ -0,0 +1,9 @@
+/**
+ * @file acceptor.h
+ * @author Sigma711 (sigma711@foxmail.com)
+ * @brief  // TODO:
+ * @date 2021-12-03
+ *
+ * @copyright Copyright (c) 2021 Sigma711
+ *
+ */
