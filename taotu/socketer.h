60b6517b6a411bd19a469e4b7c4cfdbae00bdda6
Sigma711 <1979934715@qq.com>
2021-12-04 01:46:36 +0000

    Develop Socketer module

--- taotu/socketer.h
+++ taotu/socketer.h
@@ -11,6 +11,8 @@
 #ifndef TAOTU_TAOTU_SOCKETER_H_
 #define TAOTU_TAOTU_SOCKETER_H_
 
+#include <poll.h>
+
 #include <functional>
 #include <memory>
 
@@ -27,14 +29,63 @@ class Eventer;
  */
 class Socketer : NonCopyableMovable {
  public:
-  typedef std::function<void()> NomalCallBack;
-  typedef std::function<void()> ReadCallBack;
+  typedef std::function<void()> NormalCallback;
+  typedef std::function<void(/*TODO:*/)> ReadCallback;
   typedef std::weak_ptr<Eventer> EventerPtr;
 
   Socketer(std::shared_ptr<Eventer> eventer, int fd);
   ~Socketer();
 
+  // Handle all events
+  void Work(/*TODO:*/);
+
+  void RegisterReadCallBack(ReadCallback cb);
+  void RegisterWriteCallback(NormalCallback cb);
+  void RegisterCloseCallback(NormalCallback cb);
+  void RegisterErrorCallback(NormalCallback cb);
+
+  int Fd() const;
+  int Events() const;
+
+  void ReceiveEvents(int in_events);
+
+  bool HasNoEvent() const;
+  bool HasReadEvents() const;
+  bool HasWriteEvents() const;
+
+  void EnableReadEvents();
+  void DisableReadEvents();
+  void EnableWriteEvents();
+  void DisableWriteEvents();
+  void ClearAllEvents();
+
+  std::shared_ptr<Eventer> HostEventer();
+
+  void RemoveMyself();
+
  private:
+  void UpdateEvents();
+
+  enum {
+    kNoEvent = 0,
+    kWriteEvents = POLLOUT,
+    kReadEvents = POLLIN | POLLPRI,
+  };
+
+  EventerPtr eventer_;
+
+  int fd_;
+
+  // For receiving
+  int in_events_;
+
+  // For setting
+  int out_events_;
+
+  ReadCallback read_callback_;
+  NormalCallback write_callback_;
+  NormalCallback close_callback_;
+  NormalCallback error_callback_;
 };
 
 }  // namespace taotu
