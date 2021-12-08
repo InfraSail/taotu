6c597def7a182ea40fe6178cc779781226dbdada
Sigma711 <1979934715@qq.com>
2021-12-08 17:51:05 +0000

    Create new Socketer module

---
+++ taotu/socketer.cc
@@ -0,0 +1,13 @@
+/**
+ * @file socketer.cc
+ * @author Sigma711 (sigma711 at foxmail dot com)
+ * @brief  //TODO:
+ * @date 2021-12-08
+ *
+ * @copyright Copyright (c) 2021 Sigma711
+ *
+ */
+
+#include "socketer.h"
+
+using namespace taotu;
---
+++ taotu/socketer.h
@@ -0,0 +1,24 @@
+/**
+ * @file socketer.h
+ * @author Sigma711 (sigma711 at foxmail dot com)
+ * @brief  //TODO:
+ * @date 2021-12-08
+ *
+ * @copyright Copyright (c) 2021 Sigma711
+ *
+ */
+
+#ifndef TAOTU_TAOTU_SOCKETER_H_
+#define TAOTU_TAOTU_SOCKETER_H_
+
+namespace taotu {
+
+/**
+ * @brief  // TODO:
+ *
+ */
+class Socketer {};
+
+}  // namespace taotu
+
+#endif  // !TAOTU_TAOTU_SOCKETER_H_
