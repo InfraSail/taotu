97aca0af5c723fe49b7c98dd71a20ab7a6c1f799
Sigma711 <1979934715@qq.com>
2021-12-12 12:36:10 +0000

    Create SocketAddress module

---
+++ taotu/socket_address.cc
@@ -0,0 +1,13 @@
+/**
+ * @file socket_address.cc
+ * @author Sigma711 (sigma711 at foxmail dot com)
+ * @brief  // TODO:
+ * @date 2021-12-12
+ *
+ * @copyright Copyright (c) 2021 Sigma711
+ *
+ */
+
+#include "socket_address.h"
+
+using namespace taotu;
---
+++ taotu/socket_address.h
@@ -0,0 +1,20 @@
+/**
+ * @file socket_address.h
+ * @author Sigma711 (sigma711 at foxmail dot com)
+ * @brief  // TODO:
+ * @date 2021-12-12
+ *
+ * @copyright Copyright (c) 2021 Sigma711
+ *
+ */
+
+#ifndef TAOTU_TAOTU_SOCKET_ADDRESS_H_
+#define TAOTU_TAOTU_SOCKET_ADDRESS_H_
+
+namespace taotu {
+
+class SocketAddress {};
+
+}  // namespace taotu
+
+#endif  // !TAOTU_TAOTU_SOCKET_ADDRESS_H_
