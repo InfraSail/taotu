a92786a740541a222355575f86351f23743d8ab5
Sigma711 <1979934715@qq.com>
2021-12-12 07:33:45 +0000

    Develop Connector module

---
+++ taotu/connector.cc
@@ -0,0 +1,13 @@
+/**
+ * @file connector.cc
+ * @author Sigma711 (sigma711 at foxmail dot com)
+ * @brief  // TODO:
+ * @date 2021-12-12
+ *
+ * @copyright Copyright (c) 2021 Sigma711
+ *
+ */
+
+#include "connector.h"
+
+using namespace taotu;
---
+++ taotu/connector.h
@@ -0,0 +1,16 @@
+/**
+ * @file connector.h
+ * @author Sigma711 (sigma711 at foxmail dot com)
+ * @brief  // TODO:
+ * @date 2021-12-12
+ *
+ * @copyright Copyright (c) 2021 Sigma711
+ *
+ */
+
+#ifndef TAOTU_TAOTU_CONNECTOR_H_
+#define TAOTU_TAOTU_CONNECTOR_H_
+
+namespace taotu {}  // namespace taotu
+
+#endif  // !TAOTU_TAOTU_CONNECTOR_H_
