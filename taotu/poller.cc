835b0d39c6b64479ab746c71928d4636bea2f8d9
Sigma711 <1979934715@qq.com>
2021-12-16 13:34:07 +0000

    Create Poller module

---
+++ taotu/poller.cc
@@ -0,0 +1,13 @@
+/**
+ * @file poller.cc
+ * @author Sigma711 (sigma711 at foxmail dot com)
+ * @brief  // TODO:
+ * @date 2021-12-16
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+#include "poller.h"
+
+using namespace taotu;
---
+++ taotu/poller.h
@@ -0,0 +1,24 @@
+/**
+ * @file poller.h
+ * @author Sigma711 (sigma711 at foxmail dot com)
+ * @brief  // TODO:
+ * @date 2021-12-16
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+#ifndef TAOTU_TAOTU_POLLER_H_
+#define TAOTU_TAOTU_POLLER_H_
+
+namespace taotu {
+
+/**
+ * @brief  // TODO:
+ *
+ */
+class Poller {};
+
+}  // namespace taotu
+
+#endif  // !TAOTU_TAOTU_POLLER_H_
