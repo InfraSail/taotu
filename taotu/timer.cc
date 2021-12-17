77b7f9ec16dc7abec627d45d1b5057b59918fd5d
Sigma711 <1979934715@qq.com>
2021-12-17 04:34:51 +0000

    Create Timer module

---
+++ taotu/timer.cc
@@ -0,0 +1,13 @@
+/**
+ * @file timer.cc
+ * @author Sigma711 (sigma711 at foxmail dot com)
+ * @brief  // TODO:
+ * @date 2021-12-17
+ *
+ * @copyright Copyright (c) 2021 Sigma711
+ *
+ */
+
+#include "timer.h"
+
+using namespace taotu;
---
+++ taotu/timer.h
@@ -0,0 +1,24 @@
+/**
+ * @file timer.h
+ * @author Sigma711 (sigma711 at foxmail dot com)
+ * @brief  // TODO:
+ * @date 2021-12-17
+ *
+ * @copyright Copyright (c) 2021 Sigma711
+ *
+ */
+
+#ifndef TAOTU_TAOTU_TIMER_H_
+#define TAOTU_TAOTU_TIMER_H_
+
+namespace taotu {
+
+/**
+ * @brief  // TODO:
+ *
+ */
+class Timer {};
+
+}  // namespace taotu
+
+#endif  // !TAOTU_TAOTU_TIMER_H_
