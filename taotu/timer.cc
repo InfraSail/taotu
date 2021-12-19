5f86feda7eff5b19765c8953e8618cf860e7e6d4
Sigma711 <1979934715@qq.com>
2021-12-19 02:41:18 +0000

    Develop Timer module

--- taotu/timer.cc
+++ taotu/timer.cc
@@ -10,19 +10,32 @@
 
 #include "timer.h"
 
+#include <algorithm>
 #include <utility>
 
 using namespace taotu;
-
-Timer::Timer(EventManager* event_manager) : event_manager_(event_manager) {}
 
 void Timer::AddTimeTask(TimePoint time_point, TimeCallback time_callback) {
   LockGuard lock_guard(mutex_lock_);
   time_points_.insert({time_point, std::move(time_callback)});
 }
 
+int Timer::GetMinTimeSet() const {
+  LockGuard lock_guard(mutex_lock_);
+  return time_points_.begin()->first.GetMillisecond();
+}
+
 Timer::ExpiredTimeTasks Timer::GetExpiredTimeTasks() {
   ExpiredTimeTasks expired_time_tasks;
-  TimePoint now;
+  {
+    LockGuard lock_guard(mutex_lock_);
+    TimePoints::iterator itr;
+    TimePoint now;
+    for (itr = time_points_.begin();
+         itr != time_points_.end() && itr->first <= now; ++itr) {
+      expired_time_tasks.emplace_back(itr->first, itr->second);
+    }
+    time_points_.erase(time_points_.begin(), itr);
+  }
   return expired_time_tasks;
 }
