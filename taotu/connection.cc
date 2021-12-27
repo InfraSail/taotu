57a89be71cc99acf79bf987c11e9b36e86c4231d
Sigma711 <1979934715@qq.com>
2021-12-27 20:36:05 +0000

    Develop Connection module

---
+++ taotu/connection.cc
@@ -0,0 +1,13 @@
+/**
+ * @file connection.cc
+ * @author Sigma711 (sigma711 at foxmail dot com)
+ * @brief  // TODO:
+ * @date 2021-12-27
+ *
+ * @copyright Copyright (c) 2021 Sigma711
+ *
+ */
+
+#include "connection.h"
+
+using namespace taotu;
---
+++ taotu/connection.h
@@ -0,0 +1,24 @@
+/**
+ * @file connection.h
+ * @author Sigma711 (sigma711 at foxmail dot com)
+ * @brief  // TODO:
+ * @date 2021-12-27
+ *
+ * @copyright Copyright (c) 2021 Sigma711
+ *
+ */
+
+#ifndef TAOTU_TAOTU_CONNECTION_H_
+#define TAOTU_TAOTU_CONNECTION_H_
+
+namespace taotu {
+
+/**
+ * @brief  // TODO:
+ *
+ */
+class Connection {};
+
+}  // namespace taotu
+
+#endif  // !TAOTU_TAOTU_CONNECTION_H_
