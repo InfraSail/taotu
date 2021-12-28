f44d02c5582628cf782230ca85204945807669af
Sigma711 <1979934715@qq.com>
2021-12-28 18:48:22 +0000

    Create IoBuffer module

---
+++ taotu/io_buffer.h
@@ -0,0 +1,24 @@
+/**
+ * @file io_buffer.h
+ * @author Sigma711 (sigma711 at foxmail dot com)
+ * @brief  // TODO:
+ * @date 2021-12-28
+ *
+ * @copyright Copyright (c) 2021 Sigma711
+ *
+ */
+
+#ifndef TAOTU_TAOTU_IO_BUFFER_H_
+#define TAOTU_TAOTU_IO_BUFFER_H_
+
+namespace taotu {
+
+/**
+ * @brief  // TODO:
+ *
+ */
+class IoBuffer {};
+
+}  // namespace taotu
+
+#endif  // !TAOTU_TAOTU_IO_BUFFER_H_
