c64e4fd76bd1e1a1ef08676ed6209ccba050f7e4
Sigma711 <1979934715@qq.com>
2022-02-15 14:54:17 +0000

    Develop MemoryPool module

---
+++ taotu/memory_pool.h
@@ -0,0 +1,14 @@
+/**
+ * @file memory_pool.h
+ * @author Sigma711 (sigma711 at foxmail dot com)
+ * @brief  // TODO:
+ * @date 2022-02-15
+ *
+ * @copyright Copyright (c) 2022 Sigma711
+ *
+ */
+
+#ifndef TAOTU_TAOTU_MEMORY_POOL_H_
+#define TAOTU_TAOTU_MEMORY_POOL_H_
+
+#endif  // !TAOTU_TAOTU_MEMORY_POOL_H_
