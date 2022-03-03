8d8bf62cc0bbbf32f6efd036781ee1975b8a4c9d
Sigma711 <1979934715@qq.com>
2022-03-03 02:02:36 +0000

    Develop NonCopyableMovable module

--- src/non_copyable_movable.h
+++ src/non_copyable_movable.h
@@ -9,8 +9,8 @@
  *
  */
 
-#ifndef TAOTU_TAOTU_NON_COPYABLE_MOVABLE_H_
-#define TAOTU_TAOTU_NON_COPYABLE_MOVABLE_H_
+#ifndef TAOTU_SRC_NON_COPYABLE_MOVABLE_H_
+#define TAOTU_SRC_NON_COPYABLE_MOVABLE_H_
 
 namespace taotu {
 
@@ -32,4 +32,4 @@ class NonCopyableMovable {
 
 }  // namespace taotu
 
-#endif  // !TAOTU_TAOTU_NON_COPYABLE_MOVABLE_H_
+#endif  // !TAOTU_SRC_NON_COPYABLE_MOVABLE_H_
