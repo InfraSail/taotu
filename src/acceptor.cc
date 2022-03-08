471e038573b4c5cb29fd1915143f1e8d0f30576a
Sigma711 <1979934715@qq.com>
2022-03-08 09:30:40 +0000

    Develop Acceptor module

--- src/acceptor.cc
+++ src/acceptor.cc
@@ -43,7 +43,8 @@ Acceptor::Acceptor(Poller* poller, const NetAddress& listen_address,
   accept_soketer_.SetReuseAddress(true);
   accept_soketer_.SetReusePort(should_reuse_port);
   accept_soketer_.BindAddress(listen_address);
-  accept_eventer_.RegisterReadCallback([this](const TimePoint& time_point) { this->DoReading(); });
+  accept_eventer_.RegisterReadCallback(
+      [this](const TimePoint& time_point) { this->DoReading(); });
 }
 Acceptor::~Acceptor() {
   LOG(logger::kDebug, "Acceptor with fd(" +
