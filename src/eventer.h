2c4aab9a59a8bd3330f64a3538cd4d8b456900eb
Sigma711 <1979934715@qq.com>
2022-03-25 03:23:52 +0000

    Develop Eventer module

--- src/eventer.h
+++ src/eventer.h
@@ -67,6 +67,9 @@ class Eventer : NonCopyableMovable {
   void DisableWriteEvents();
   void DisableAllEvents();
 
+  // Only used in Connector::RemoveAndReset() (prepare for EventerPtr::reset())
+  void GetReadyDestroy() { is_handling_ = false; }
+
   void RemoveMyself();
 
  private:
