f152dbdac7250e6358668243b16941cc946845d8
Sigma711 <1979934715@qq.com>
2022-03-31 00:52:16 +0000

    Develop examples

--- example/pingpong/pingpong_client.h
+++ example/pingpong/pingpong_client.h
@@ -11,8 +11,71 @@
 #ifndef TAOTU_EXAMPLE_PINGPONG_PINGPONG_CLIENT_H_
 #define TAOTU_EXAMPLE_PINGPONG_PINGPONG_CLIENT_H_
 
+#include <atomic>
+#include <memory>
+#include <string>
+#include <vector>
+
 #include "../../src/client.h"
 
-class PingpongClient : taotu::NonCopyableMovable {};
+class Session;
+
+/**
+ * @brief  // TODO:
+ *
+ */
+class PingpongClient : taotu::NonCopyableMovable {
+ public:
+  PingpongClient(const taotu::NetAddress& server_address, int block_size,
+                 int session_count, int timeout, int thread_count);
+  ~PingpongClient();
+
+  void Start() { event_manager_->Work(); }
+
+  const std::string& GetMessage() const { return message_; }
+
+  void OnConnecting();
+  void OnDisconnecting(taotu::Connecting& connection);
+
+ private:
+  typedef std::vector<taotu::EventManager*> EventManagers;
+
+  void DoWithTimeout();
+
+  std::unique_ptr<taotu::EventManager> event_manager_;
+  EventManagers event_managers_;
+  int session_count_;
+  int timeout_;
+  std::vector<std::unique_ptr<Session>> sessions_;
+  std::string message_;
+  std::atomic_int32_t conn_num_;
+  std::unique_ptr<taotu::Balancer> balancer_;
+};
+
+/**
+ * @brief  // TODO:
+ *
+ */
+class Session : taotu::NonCopyableMovable {
+ public:
+  Session(taotu::EventManager* event_manager,
+          const taotu::NetAddress& server_address, PingpongClient* client);
+
+  void Start();
+  void Stop();
+
+  int64_t GetBytesRead() const { return bytes_read_; }
+  int64_t GetMessagesRead() const { return messages_read_; }
+
+ private:
+  void OnConnectionCallback(taotu::Connecting& connection);
+  void OnMessageCallback(taotu::Connecting& connection,
+                         taotu::IoBuffer* io_buffer, taotu::TimePoint);
+
+  taotu::Client client_;
+  PingpongClient* master_;
+  int64_t bytes_read_;
+  int64_t messages_read_;
+};
 
 #endif  // !TAOTU_EXAMPLE_PINGPONG_PINGPONG_CLIENT_H_
