b199ed7dac9fad0e34ce507edcdd22a71769bfa4
Sigma711 <1979934715@qq.com>
2022-04-19 14:35:00 +0000

    Develop ObjectPool module

--- src/object_pool.h
+++ src/object_pool.h
@@ -28,6 +28,7 @@ class ObjectPool : NonCopyableMovable {
   ObjectPool() {}
   ~ObjectPool() {}
 
+  // New an object
   template <class... Args>
   T* New(Args... args) {
     return NewIf(
@@ -36,6 +37,7 @@ class ObjectPool : NonCopyableMovable {
         args...);
   }
 
+  // Delete an object
   void Delete(void* object_ptr) {
     if (nullptr == object_ptr) {
       return;
@@ -47,28 +49,33 @@ class ObjectPool : NonCopyableMovable {
   }
 
  private:
+  // New an object when it is trivially constructible
   template <typename... Args>
   T* NewIf(std::true_type, Args... args) {
     return reinterpret_cast<T*>(memory_pool_.Allocate());
   }
 
+  // New an object when it is not trivially constructible
   template <typename... Args>
   T* NewIf(std::false_type, Args... args) {
     void* new_object_ptr = memory_pool_.Allocate();
     return new (new_object_ptr) T(args...);
   }
 
+  // Delete an object when it is trivially constructible
   template <typename... Args>
   void DeleteIf(std::true_type, void* object_ptr) {
     memory_pool_.Deallocate(object_ptr);
   }
 
+  // Delete an object when it is not trivially constructible
   template <typename... Args>
   void DeleteIf(std::false_type, void* object_ptr) {
     reinterpret_cast<T*>(object_ptr)->~T();
     memory_pool_.Deallocate(object_ptr);
   }
 
+  // Memory pool
   MemoryPool<sizeof(T)> memory_pool_;
 };
 
