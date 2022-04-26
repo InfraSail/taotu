46121cf83879db6d6fa49d6412d0c39ac1efbbe4
Sigma711 <1979934715@qq.com>
2022-04-27 00:14:12 +0100

    Develop unittest

--- test/time_unittest.cc
+++ test/time_unittest.cc
@@ -5,9 +5,23 @@
 #include "../src/time_point.h"
 #include "../src/timer.h"
 
-TEST(TimeTest, AddTimeTaskTest) {
+TEST(TimeTest, TimePointTest) {
+  taotu::TimePoint time_point1;
+  taotu::TimePoint time_point2{4 * 1000 * 1000};
+  taotu::TimePoint time_point3{3 * 1000 * 1000};
+  taotu::TimePoint time_point4 = time_point3;
+  ASSERT_TRUE(time_point1 < time_point2);
+  ASSERT_FALSE(time_point2 <= time_point3);
+  ASSERT_TRUE(time_point3 == time_point4);
+  ASSERT_TRUE(time_point3 <= time_point4);
+  auto res1 = static_cast<int>(time_point2.GetMicroseconds() / 1000);
+  auto res2 = static_cast<int>(time_point2.GetMillisecond());
+  ASSERT_EQ(res1, res2);
+}
+
+TEST(TimeTest, TimerTest) {
   taotu::Timer timer;
-  EXPECT_EQ(timer.GetMinTimeDuration(), 10000);
+  ASSERT_EQ(timer.GetMinTimeDuration(), 10000);
   int flag1 = 0;
   int flag2 = 0;
   int flag3 = 0;
@@ -20,20 +34,20 @@ TEST(TimeTest, AddTimeTaskTest) {
   auto f3 = [&]() { flag3 = 3; };
   taotu::TimePoint time_point3;
   timer.AddTimeTask(time_point3, std::move(f3));
-  EXPECT_EQ(timer.GetMinTimeDuration(), 0);
+  ASSERT_EQ(timer.GetMinTimeDuration(), 0);
   ::sleep(4);
   auto time_task_pair_list = timer.GetExpiredTimeTasks();
-  EXPECT_EQ(time_task_pair_list[0].first.GetMillisecond(),
+  ASSERT_EQ(time_task_pair_list[0].first.GetMillisecond(),
             time_point3.GetMillisecond());
   time_task_pair_list[0].second();
-  EXPECT_EQ(flag3, 3);
-  EXPECT_EQ(time_task_pair_list[1].first.GetMillisecond(),
+  ASSERT_EQ(flag3, 3);
+  ASSERT_EQ(time_task_pair_list[1].first.GetMillisecond(),
             time_point2.GetMillisecond());
   time_task_pair_list[1].second();
-  EXPECT_EQ(flag2, 2);
-  EXPECT_EQ(time_task_pair_list[2].first.GetMillisecond(),
+  ASSERT_EQ(flag2, 2);
+  ASSERT_EQ(time_task_pair_list[2].first.GetMillisecond(),
             time_point1.GetMillisecond());
   time_task_pair_list[2].second();
-  EXPECT_EQ(flag1, 1);
-  EXPECT_EQ(timer.GetMinTimeDuration(), 10000);
+  ASSERT_EQ(flag1, 1);
+  ASSERT_EQ(timer.GetMinTimeDuration(), 10000);
 }
